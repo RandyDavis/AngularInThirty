08180ee9925286476d9b75fcd7a59182bb9ae215
Randy Davis <randy_davis@outlook.com>
2014-12-26 23:59:04 -0600

    add additional filters

--- main.ctrl.js
+++ main.ctrl.js
@@ -3,7 +3,7 @@ angular.module('app')
     var vm = this;
     vm.title = "AngularJS Tut in 30 minutes";
     vm.searchInput = '';
-    vm.shows= [
+    vm.shows = [
       {
         title: 'Game of Thrones',
         year: '2011',
@@ -30,4 +30,59 @@ angular.module('app')
         favorite: false
       }
     ];
+    vm.orders = [
+      {
+        id: 1,
+        title: 'Year Ascending',
+        key: 'year',
+        reverse: false
+      },
+      {
+        id: 2,
+        title: 'Year Descending',
+        key: 'year',
+        reverse: true
+      },
+      {
+        id: 3,
+        title: 'Title Ascending',
+        key: 'title',
+        reverse: false
+      },
+      {
+        id: 4,
+        title: 'Title Descending',
+        key: 'title',
+        reverse: true
+      },
+    ];
+    vm.order = vm.orders[0];
   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
